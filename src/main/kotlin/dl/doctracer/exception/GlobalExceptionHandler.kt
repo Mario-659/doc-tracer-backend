9a994748afbac2a0b048251f1f56eefa90f27695
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-09-01 18:38:28 +0200

    handle method not allowed exception

--- src/main/kotlin/dl/doctracer/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/dl/doctracer/exception/GlobalExceptionHandler.kt
@@ -1,14 +1,19 @@
 package dl.doctracer.exception
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    private val logger: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Any> {
@@ -26,8 +31,14 @@ class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleIllegalArgumentException(ex: HttpRequestMethodNotSupportedException): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(ex.message)
+    }
+
     @ExceptionHandler(Exception::class)
     fun handleAllExceptions(ex: Exception): ResponseEntity<Any> {
+        logger.error(ex.message)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred")
     }
 }
