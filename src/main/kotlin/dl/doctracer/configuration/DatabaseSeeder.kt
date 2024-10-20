f29561ac9d7116c94d9755acf81c6abf8a7a4e43
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-20 19:10:01 +0200

    add more data on init

--- src/main/kotlin/dl/doctracer/configuration/DatabaseSeeder.kt
+++ src/main/kotlin/dl/doctracer/configuration/DatabaseSeeder.kt
@@ -29,52 +29,71 @@ class DatabaseSeeder {
         coveringMaterialService: CoveringMaterialService
     ): CommandLineRunner {
         return CommandLineRunner {
-            logger.info("Initializing database with mock data")
+            logger.info("Initializing database with authentic mock data")
 
             val registerRequests = listOf(
-                RegisterRequest("admin", "password1", "admin1@gmail.com", "adminName", "adminSurname"), // TODO update when admin registration is implemented
-                RegisterRequest("user1", "password1", "user1@gmail.com", "adminName", "adminSurname")
+                RegisterRequest("admin", "password1", "admin1@gmail.com", "Admin", "Administrator"),
+                RegisterRequest("user1", "password1", "user1@gmail.com", "User", "One"),
+                RegisterRequest("user2", "password2", "user2@gmail.com", "User", "Two")
             )
             val users = registerRequests.map { userAuthService.register(it) }
 
             var devices = listOf(
-                Device(null, "Device A", "Description of Device A", "Manufacturer A"),
-                Device(null, "Device B", "Description of Device B", "Manufacturer B")
+                Device(null, "VSC800", "Versatile spectral comparator", "Foster + Freeman"),
+                Device(null, "LC-MS-200", "Liquid Chromatography Mass Spectrometry", "Shimadzu"),
+                Device(null, "NMR-500", "Nuclear Magnetic Resonance Spectrometer", "Bruker"),
+                Device(null, "UV-VIS-Pro", "UV-Visible Spectrophotometer", "Agilent"),
+                Device(null, "RAMAN-Scope", "Raman Spectrometer", "Renishaw")
             )
             devices = devices.map { deviceService.save(it) }
 
             var spectraTypes = listOf(
-                SpectraType(null, "Type A", "Description of Type A"),
-                SpectraType(null, "Type B", "Description of Type B")
+                SpectraType(null, "IR", "Infrared Spectroscopy"),
+                SpectraType(null, "GC-MS", "Gas Chromatography Mass Spectrometry"),
+                SpectraType(null, "LC-MS", "Liquid Chromatography Mass Spectrometry"),
+                SpectraType(null, "NMR", "Nuclear Magnetic Resonance"),
+                SpectraType(null, "RAMAN", "Raman Spectroscopy"),
+                SpectraType(null, "UV-VIS", "Ultraviolet-Visible Spectroscopy")
             )
             spectraTypes = spectraTypes.map { spectraTypeService.save(it) }
 
             var coveredMaterials = listOf(
-                CoveredMaterial(null, "Material A", "Description of Material A"),
-                CoveredMaterial(null, "Material B", "Description of Material B")
+                CoveredMaterial(null, "Ballpoint Ink", "Standard black ballpoint pen ink"),
+                CoveredMaterial(null, "Gel Pen Ink", "Blue gel pen ink"),
+                CoveredMaterial(null, "Laser Toner", "Black laser printer toner"),
+                CoveredMaterial(null, "Fountain Pen Ink", "Green fountain pen ink"),
+                CoveredMaterial(null, "Stamp Ink", "Red stamp ink")
             )
             coveredMaterials = coveredMaterials.map { coveredMaterialService.save(it) }
 
             var coveringMaterials = listOf(
-                CoveringMaterial(null, "Covering A", "Description of Covering A", "Manufacturer A", "Red"),
-                CoveringMaterial(null, "Covering B", "Description of Covering B", "Manufacturer B", "Blue")
+                CoveringMaterial(null, "Covering A", "Matte black toner", "HP", "Black"),
+                CoveringMaterial(null, "Covering B", "Glossy red ink", "Pilot", "Red"),
+                CoveringMaterial(null, "Covering C", "Waterproof blue ink", "Pentel", "Blue"),
+                CoveringMaterial(null, "Covering D", "Vibrant green ink", "Lamy", "Green"),
+                CoveringMaterial(null, "Covering E", "Quick-dry black ink", "Uni-ball", "Black")
             )
             coveringMaterials = coveringMaterials.map { coveringMaterialService.save(it) }
 
             var samples = listOf(
                 Sample(null, "Sample A", Instant.now(), coveredMaterials[0], coveringMaterials[0], users[0]),
                 Sample(null, "Sample B", Instant.now(), coveredMaterials[1], coveringMaterials[1], users[1]),
+                Sample(null, "Sample C", Instant.now(), coveredMaterials[2], coveringMaterials[2], users[2]),
+                Sample(null, "Sample D", Instant.now(), coveredMaterials[3], coveringMaterials[3], users[0]),
+                Sample(null, "Sample E", Instant.now(), coveredMaterials[4], coveringMaterials[4], users[1])
             )
             samples = samples.map { sampleRepository.save(it) }
 
             val spectra = listOf(
-                Spectra(null, "{ \"spectrumSample\": [1,2,3] }", Instant.now(), spectraTypes[0], devices[0], samples[0], users[0]),
-                Spectra(null, "{ \"spectrumSample\": [4,5,6] }", Instant.now(), spectraTypes[1], devices[1], samples[1], users[1])
+                Spectra(null, "{ \"spectrumSample\": [100,200,300] }", Instant.now(), spectraTypes[0], devices[0], samples[0], users[0]),
+                Spectra(null, "{ \"spectrumSample\": [150,250,350] }", Instant.now(), spectraTypes[1], devices[1], samples[1], users[1]),
+                Spectra(null, "{ \"spectrumSample\": [200,300,400] }", Instant.now(), spectraTypes[2], devices[2], samples[2], users[2]),
+                Spectra(null, "{ \"spectrumSample\": [120,220,320] }", Instant.now(), spectraTypes[3], devices[3], samples[3], users[0]),
+                Spectra(null, "{ \"spectrumSample\": [130,230,330] }", Instant.now(), spectraTypes[4], devices[4], samples[4], users[1])
             )
             spectra.forEach { spectraRepository.save(it) }
 
-            logger.info("Mock data has been initialized")
+            logger.info("Authentic mock data has been initialized")
         }
     }
 }
-
