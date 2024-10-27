053544679e11b0e822f668b04381581cb91b3700
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2024-10-27 18:01:01 +0100

    add todo

--- src/main/kotlin/dl/doctracer/controller/AdminUserController.kt
+++ src/main/kotlin/dl/doctracer/controller/AdminUserController.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
+// TODO block update requests for user requesting that so that someone would not block himself
 @RestController
 @RequestMapping("/admin/users")
 @PreAuthorize("hasRole('ADMIN')")
